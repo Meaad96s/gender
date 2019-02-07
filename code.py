c43c9cceac0dbc2264cfa69ea0fc2835d6208f4c
Meaad96s <meaad_r@hotmail.com>
2019-02-07 11:32:05 +0300

    def

--- code.py
+++ code.py
@@ -2,46 +2,54 @@ import cv2
 import glob
 #import face_alignment
 from skimage import io
-from sklearn import preprocessing
+#from sklearn import preprocessing
 
 #def main() :
     # Read RGB image
-females = [cv2.imread(file) for file in glob.glob('C:\\Users\\FatenAldawish\\Documents\\GitHub\\gender\\Female_Dataset\\*.jpg')]
-males = [cv2.imread(file) for file in glob.glob('C:\\Users\\FatenAldawish\\Documents\\GitHub\\gender\\Male_Dataset\\*.jpg')]
+def readimages():
+    females = [cv2.imread(file) for file in glob.glob('C:\\Users\\MeaadAlrshoud\\Documents\\GitHub\\gender\\Female_Dataset\\*.jpg')]
+    males = [cv2.imread(file) for file in glob.glob('C:\\Users\\MeaadAlrshoud\\Documents\\GitHub\\gender\\Male_Dataset\\*.jpg')]
+    return females,males
+
+
 
 #convert to greyscale
+def convertygrey(l1,l2):
+    f_grey= [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in l1]
+    m_grey=[cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in l2]
+    return f_grey,m_grey
 
-f_grey= [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in females]
-m_grey=[cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in males]
-count=0
+
 # print the images
 
 # Normalization
-normalized_f = [cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in f_grey]
-normalized_m =[cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in m_grey]
+def normalization (l1,l2):
+    normalized_f = [cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in l1]
+    normalized_m =[cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in l2]
+    return normalized_f,normalized_m
+
 
 # Haar
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Detect faces in the image
-f_haar= [ face_cascade.detectMultiScale(
+def haar(l1,l2):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # Detect faces in the image
+    f_haar= [ face_cascade.detectMultiScale(
     img,
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
     flags = cv2.CASCADE_SCALE_IMAGE
-    ) for img in normalized_f  ]
+    ) for img in l1  ]
 
-m_haar=[face_cascade.detectMultiScale(
-    img,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
-    ) for img in normalized_m]
+    m_haar=[face_cascade.detectMultiScale(img,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE) for img in l2]
 
-# print haar affect
-print("Found {0} faces!".format(len(f_haar)))
-print("Found {0} faces!".format(len(m_haar)))
+    return f_haar, m_haar
+
+
 
 # Face Alignment
 
@@ -49,8 +57,13 @@ print("Found {0} faces!".format(len(m_haar)))
 
 #input = io.imread('../test/assets/aflw-test.jpg')
 #preds = fa.get_landmarks(m_haar)
+def main():
+    females,males=readimages()
+    females,males=convertygrey(females,males)
+    females,males=normalization(females,males)
+    females,males=haar(females,males)
+    print("Found {0} faces!".format(len(females)))
+    print("Found {0} faces!".format(len(males)))
 
-
-    #if __name__ == "__main__":
-    #    main()
-    # execute only if run as a script
+if __name__ == "__main__":
+    main()
