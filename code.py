d867e472c354fc6a811b531f6d5204c969075a8e
FatenAldawish <42312407+FatenAldawish@users.noreply.github.com>
2019-02-07 11:00:28 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
 import cv2
 import glob
+#import face_alignment
+from skimage import io
+from sklearn import preprocessing
 
 #def main() :
     # Read RGB image
-females = [cv2.imread(file) for file in glob.glob('C:\\Users\\MeaadAlrshoud\\Documents\\GitHub\\Gender-Classification\\Female_Dataset\\*.jpg')]
-males = [cv2.imread(file) for file in glob.glob('C:\\Users\\MeaadAlrshoud\\Documents\\GitHub\\Gender-Classification\\Male_Dataset\\*.jpg')]
+females = [cv2.imread(file) for file in glob.glob('C:\\Users\\FatenAldawish\\Documents\\GitHub\\gender\\Female_Dataset\\*.jpg')]
+males = [cv2.imread(file) for file in glob.glob('C:\\Users\\FatenAldawish\\Documents\\GitHub\\gender\\Male_Dataset\\*.jpg')]
 
 #convert to greyscale
 
@@ -14,8 +17,8 @@ count=0
 # print the images
 
 # Normalization
-
-
+normalized_f = [cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in f_grey]
+normalized_m =[cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)for img in m_grey]
 
 # Haar
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -26,7 +29,7 @@ f_haar= [ face_cascade.detectMultiScale(
     minNeighbors=5,
     minSize=(30, 30),
     flags = cv2.CASCADE_SCALE_IMAGE
-    ) for img in f_grey]
+    ) for img in normalized_f  ]
 
 m_haar=[face_cascade.detectMultiScale(
     img,
@@ -34,19 +37,18 @@ m_haar=[face_cascade.detectMultiScale(
     minNeighbors=5,
     minSize=(30, 30),
     flags = cv2.CASCADE_SCALE_IMAGE
-    ) for img in m_grey]
+    ) for img in normalized_m]
 
 # print haar affect
 print("Found {0} faces!".format(len(f_haar)))
 print("Found {0} faces!".format(len(m_haar)))
 
 # Face Alignment
-import face_alignment
-from skimage import io
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+
+#fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 
 #input = io.imread('../test/assets/aflw-test.jpg')
-preds = fa.get_landmarks(m_haar)
+#preds = fa.get_landmarks(m_haar)
 
 
     #if __name__ == "__main__":
