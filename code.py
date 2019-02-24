456996b99eae8a661c496e81684889c93f993d4b
FatenAldawish <42312407+FatenAldawish@users.noreply.github.com>
2019-02-24 23:29:51 +0300

    retrive Meaad's code

--- code.py
+++ code.py
@@ -61,43 +61,119 @@ def spatial_scale(l1,l2):
 #def face_alignment(l1,l2):
 #    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 #    input = io.imread('../test/assets/aflw-test.jpg')
-#preds = fa.get_landmarks(m_haar)
+# preds = fa.get_landmarks(m_haar)
 
-def annotation (l1,l2):
-    #ones= [1 for img in l1]
-    a= ones([len(l1),1])
-    #zeros=[0 for img in l2]
-    #print(a)
-    #np.append(l1,a)
-    #l1.append(a)
+# def spatial-scale():
 
-    #print(l1)
+def annotation(l1, l2):
 
-    #np.append(l2,zeros,axis=1)
-    return l1,l2
+	f = [np.expand_dims(file,0) for file in l1]
+	m = [np.expand_dims(file,0) for file in l2]
+	#ones= [1 for img in l1]
+	#print(len(ones))
+	print("L1")
+	#print(l1)
+	label1 = ones((len(l1), 1),dtype='float')
+	label2= zeros((len(l2), 1),dtype='float')
+	#zeros=[0 for img in l2]
+	print(label1.shape)
+	print(l1)
+	np.column_stack((l1,label1))
+	# l1.append(a)
+	#np.append(l2,label2,axis=1)
+	#print(label2.shape,l1.shape)
+	return l1, l2
 
+
+def hog(l1):
+	winSize = (64, 64)
+	blockSize = (16, 16)
+	blockStride = (8, 8)
+	cellSize = (8, 8)
+	nbins = 9
+	derivAperture = 1
+	winSigma = 4.
+	histogramNormType = 0
+	L2HysThreshold = 2.0000000000000001e-01
+	gammaCorrection = 0
+	nlevels = 64
+	hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
+							histogramNormType, L2HysThreshold, gammaCorrection, nlevels)
+	# compute(img[, winStride[, padding[, locations]]]) -> descriptors
+	winStride = (8, 8)
+	padding = (8, 8)
+	locations = ((10, 20),)
+	f_hog = [hog.compute(img, winStride, padding, locations) for img in l1]
+	# Rescale histogram for better display
+	# hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+	return f_hog
 
 def lbp(l1,l2):
-    radius = 3
-    n_points = 8 * radius
-    f_lbp= [local_binary_pattern(img, n_points, radius, method="uniform") for img in l1]
-    m_lbp=[local_binary_pattern(img, n_points, radius, method="uniform")for img in l2]
-    return f_lbp,m_lbp
+	radius = 3
+	n_points = 8 * radius
+	f_lbp= [local_binary_pattern(img, n_points, radius, method="uniform") for img in l1]
+	m_lbp=[local_binary_pattern(img, n_points, radius, method="uniform")for img in l2]
+	return f_lbp,m_lbp
+
+def dr_pca(x_train):
+	# X_std = StandardScaler().fit_transform(X)
+	# mean_vec = np.mean(X_std, axis=0)
+	# cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
+	# print('Covariance matrix \n%s' %cov_mat)
+	# Y = sklearn_pca.fit_transform(X_std)
+
+	# find the principal components
+	N_COMPONENTS = 2
+	pca = PCA(n_components=N_COMPONENTS, random_state=0, svd_solver='randomized')
+	X_train_pca = pca.fit_transform(x_train)
+	# X_test_pca = pca.transform(X_test)
+
+	'''pcaclf = clf.fit(X_train_pca, y_train)
+	print("pca test score", pcaclf.score(X_test_pca, y_test))
+	print("pca train score", pcaclf.score(X_train_pca, y_train))'''
+	# X_r = pca.fit(X).transform(X)
+	# Percentage of variance explained for each components
+	print('explained variance ratio (first two components): %s' % str(pca.explained_variance_ratio_))
+
+	# def LDA():
+	'''females,males= LDA(females,males)
+	lda = LinearDiscriminantAnalysis(n_components=2)
+	X_r2 = lda.fit(X, y).transform(X)
+
+	X_train_lda = lda.fit_transform(X_train, y_train)
+	X_test_lda = lda.transform(X_test)
+
+	ldaclf = clf.fit(X_train_lda, y_train)
+	print("lda train score", ldaclf.score(X_train_lda, y_train))
+	print("lda test score", ldaclf.score(X_test_lda, y_test))'''
+
+def svm(X_train, X_test, y_train, y_test):
+	clf=svm.SVC(gamma='auto')
+	y_pred = clf.fit(X_train, y_train).predict(X_test)
+	scores = cross_val_score(clf, X, y, cv=10)
+	print (scores)
+
+def experiment1():
+	females, males = readimages()
+	females, males = convertygrey(females, males)
+	f = np.asarray(females, dtype=np.float32)
+	m = np.asarray(males, dtype=np.float32)
+	females, males = annotation(f, m)
+	print(females)
 
 def main():
-    females,males=readimages()
-    females,males=convertygrey(females,males)
-    #print(males)
-    females,males=normalization(females,males) #all are 1
-    #print(males)
-    #females,males=haar(females,males)
-    #print(males[0])
-    #print("Found {0} faces!".format(len(females)))
-    #print("Found {0} faces!".format(len(males)))
-    #females,males=annotation(females,males)
-    #print(females[0])
-    females,males=lbp(females,males)
-    print(males)
+	experiment1()
+	# print(males)
+	#females, males = normalization(females, males)  # all are 1
+	# print(males)
+	# females,males=haar(females,males)
+	# print(males)
+	# print("Found {0} faces!".format(len(females)))
+	# print("Found {0} faces!".format(len(males)))
+
+
+
+
 
 if __name__ == "__main__":
     main()
